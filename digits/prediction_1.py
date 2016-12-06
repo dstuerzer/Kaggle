ccd54b2881f5fc656443e25e42e48dfeed7cdc2d
dstuerzer <dominik.stuerzer@gmail.com>
2016-12-06 17:19:05 -0500

    better structured

--- digits/prediction_1.py
+++ digits/prediction_1.py
@@ -13,24 +13,52 @@ import matplotlib.colors as colors
 from sklearn.neural_network import MLPClassifier, MLPRegressor
 from sklearn.feature_selection import RFECV
 
+name_train = "train.csv"
+name_test = "test.csv"
+output_name = 'sol_0.csv'
 
-########################
-# prepare training data -> X, y
-########################
+n_pca = 50		# 50 seems optimal
+			# compression method:
+use_pca = 1		# 0... rfc for reduction, 
+				# 1... pca + whitening 
+				# 2... for scaling without pca
+name_estimator = "NEURAL_NW"
+par_estimator = 200
+
+# only for <rfc for reduction>:
+par_rcf_reducer = 100	# only needed if use_pca = 0
+epsilon = 0.002		#cutoff in <rfc for reduction>
 
 
 
+def RFC_f(X, y, r):
+	tri = RandomForestClassifier(r)
+	sc = cross_val_score(tri , X, y, cv=5)
+	tri.fit(X, y)
+	print('CV', np.mean(sc),tri.score(X,y))
+	G=tri.feature_importances_
+	return G, tri
 
+def SUPP_VM_rbf(X,y,c):
+	g = 0.02
+	SVM = svm.SVC(kernel='rbf', C=c, gamma = g)
+	sc = cross_val_score(SVM, X, y, cv=5)
+	SVM.fit(X, y)
+	print(np.mean(sc), SVM.score(X,y))
+	return SVM
 
-script_dir = os.path.dirname(__file__) #<-- absolute dir the script is in
-rel_path = "train.csv"
-abs_file_path = os.path.join(script_dir, rel_path)
-df =pd.read_csv(abs_file_path)
+def NEURAL_NW(X,y, c):
+	lay_size = (c,)
+	nnw=MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=lay_size)
+	sc = cross_val_score(nnw, X, y, cv=5)
+	nnw.fit(X, y)
+	print('CV', np.mean(sc), nnw.score(X,y))	
+	return nnw
 
-rel_path = "test.csv"
-abs_file_path = os.path.join(script_dir, rel_path)
-df_test =pd.read_csv(abs_file_path)
-
+def check_manually(X_test, y_pred):
+	for i in range(100):
+		print("pred", y_pred[i])
+		visualize_image(X_test[i])
 
 def visualize_image(L):
 # L is a row from dataframe
@@ -45,66 +73,66 @@ def visualize_image(L):
 	plt.show()
 
 
+
+########################
+# prepare training data -> X, y
+########################
+
+
+script_dir = os.path.dirname(__file__) #<-- absolute dir the script is in
+rel_path = name_train
+abs_file_path = os.path.join(script_dir, rel_path)
+df =pd.read_csv(abs_file_path)
+
+rel_path = name_test
+abs_file_path = os.path.join(script_dir, rel_path)
+df_test =pd.read_csv(abs_file_path)
+
 y = df['label'].tolist()
 del df['label']
 X = df.values.tolist()
 
 X_test = df_test.values.tolist()
 
-
-
-# PCA
-
-pca = PCA(n_components=50, whiten = True)
-pca.fit(X)
-X = pca.transform(X)
-X_test_pca = pca.transform(X_test)
-
-# just normalizing instead
-# scaler = preprocessing.StandardScaler().fit(X)
-# X = scaler.transform(X)
-# X_test_pca = scaler.transform(X_test)
-
 print(len(y))
 
-def RFC_f(X, y):
-	tri = RandomForestClassifier()
-	sc = cross_val_score(tri , X, y, cv=5)
-	tri.fit(X, y)
-	print('CV', np.mean(sc),tri.score(X,y))
-	#G=tri.feature_importances_
-	return tri
 
-def SUPP_VM_rbf(X,y):
-	c = 100
-	g = 0.02
-	SVM = svm.SVC(kernel='rbf', C=c, gamma = g)
-	sc = cross_val_score(SVM, X, y, cv=5)
-	SVM.fit(X, y)
-	print(np.mean(sc), SVM.score(X,y))
-	return SVM
-
-def NEURAL_NW(X,y):
-	lay_size = (2000,)
-	nnw=MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=lay_size)
-	sc = cross_val_score(nnw, X, y, cv=5)
-	nnw.fit(X, y)
-	print('CV', np.mean(sc), nnw.score(X,y))	
-	return nnw
-
-def check_manually(X_test, y_pred):
-	for i in range(100):
-		print("pred", y_pred[i])
-		visualize_image(X_test[i])
+if use_pca == 0:
+# use rfc to reduce data dimension
+	G, estimator = RFC_f(X, y, par_rcf_reducer)
+	index_G = [i for i in range(len(G)) if G[i] > epsilon]
+	print("length index_G", len(index_G))
+	X_cpr = []
+	for x in X:
+		X_cpr.append([x[i] for i in index_G])
+	scaler = preprocessing.StandardScaler().fit(X_cpr)
+	X = scaler.transform(X_cpr)
+	X_cpr_test = []
+	for x in X_test:
+		X_cpr_test.append([x[i] for i in index_G])
+	X_test_pca = scaler.transform(X_cpr_test)
+elif use_pca == 1:
+	pca = PCA(n_components=n_pca, whiten = True)
+	pca.fit(X)
+	X = pca.transform(X)
+	X_test_pca = pca.transform(X_test)
+else:
+	scaler = preprocessing.StandardScaler().fit(X)
+	X = scaler.transform(X)
+	X_test_pca = scaler.transform(X_test)
 
 
-estimator = SUPP_VM_rbf(X, y)
+for r in [50,100,200,400,600,1000]:
+	if name_estimator != "RFC_f":
+		estimator = globals()[name_estimator](X, y, par_estimator)
+	else:
+		G, estimator = RFC_f(X,y, par_estimator)
 y_pred = estimator.predict(X_test_pca)
 
 daten = {'ImageId': [i+1 for i in range(len(y_pred))] , 'Label': y_pred}
 cols = ['ImageId', 'Label']
 
 ddf = pd.DataFrame(daten, columns = cols)
-ddf.to_csv('sol.csv', index= False)
+ddf.to_csv(output_name, index= False)
 
 # check_manually(X_test, y_pred)
