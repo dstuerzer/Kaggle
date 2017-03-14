4f629995bbadbade96ec389de871ed7505986dab
dstuerzer <dominik.stuerzer@gmail.com>
2017-03-14 17:26:22 -0400

    replaced jupyter nb with optimized log regression

---
+++ credit_card_fraud/plot_confusion.py
@@ -0,0 +1,49 @@
+import itertools
+import matplotlib.pyplot as plt
+import numpy as np
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+#    else:
+#        print('Confusion matrix, without normalization')
+
+#    print(cm)
+
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j],
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.show()
+
+def show_data(cm, title='Confusion matrix', print_res = 0):
+    tp = cm[1,1]
+    fn = cm[1,0]
+    fp = cm[0,1]
+    tn = cm[0,0]
+    if print_res == 1:
+        print(title+'\n')
+        print('Precision =     {:.3f}'.format(tp/(tp+fp)))
+        print('Recall (TPR) =  {:.3f}'.format(tp/(tp+fn)))
+        print('Fallout (FPR) = {:.3e}'.format(fp/(fp+tn)))
+    return tp/(tp+fp), tp/(tp+fn), fp/(fp+tn)
